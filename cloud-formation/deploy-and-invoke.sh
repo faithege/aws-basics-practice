a3446bd214e17baa0b2c9e3b61ca588db42d7062
Faith Ege <faithege@hotmail.co.uk>
2020-08-04 23:42:23 +0100

    Update shell script to send lambda changes to S3 bucket cfn template now points at a lambda folder with multiple lambda js scripts in deploy and invoke shell script has additional command which will send these lambdas to S3 bucket

--- cloud-formation/deploy-and-invoke.sh
+++ cloud-formation/deploy-and-invoke.sh
@@ -9,17 +9,24 @@ PAYLOAD=$3 #the JSON object needs to be passed in with ''
 
 # [] are called test if want look up commands/flags type man test
 # essentially creating a custom error message if one of the three parameters are missing
-# -o checks that file exists
+# -o OR
 # -r checks that file is readable
-# -z checks that string exists
-if [ -z "${STACK}" -o -z -r "${TEMPLATE}" -z "${PAYLOAD}"]
+# -z checks for 0 lengtth strings
+# Very important to have spaces inbetween flags/arguments when using tests []
+# Can also only have one flag per argument - e.g. can't have -z and -r ahead of "${TEMPLATE}" 
+# Custom error message created if nothing provided for stack/payload and/or template unreadable
+if [ -z "${STACK}" -o ! -r "${TEMPLATE}" -o -z "${PAYLOAD}" ] 
 then
     echo "Usage: $0 <stack> <template-file> <payload>"
     exit 1
 fi
 
+WORKING_TEMPLATE="../working/output-template.cfn.yaml" # quotation marks not necessarily needed, but safer
+BUCKET=cbf-faith-bucket #in bash don't use space around equals
+
+aws cloudformation package --template-file ${TEMPLATE} --s3-bucket ${BUCKET} --output-template-file ${WORKING_TEMPLATE}
 # No need to use && as in a script we have set -e 
-aws cloudformation deploy --template-file ${TEMPLATE} --stack-name ${STACK} --capabilities CAPABILITY_IAM 
+aws cloudformation deploy --template-file ${WORKING_TEMPLATE} --stack-name ${STACK} --capabilities CAPABILITY_IAM 
 # Here we are extracting the 'Function' resource from the stack so we don't need to hardcode it -> DYNAMIC ->use $(<command>) or will be read as a string!
 # Destcribe-stack-resource returns a JSON which we interrogrape with jq to get the id we need
 FUNCTION=$(aws cloudformation describe-stack-resource --stack-name ${STACK} --logical-resource-id Function | jq -r .StackResourceDetail.PhysicalResourceId)
