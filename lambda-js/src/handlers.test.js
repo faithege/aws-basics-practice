8cdd6386b4e6982e8171087456346bc0cbf5b9f0
Faith Ege <faithege@hotmail.co.uk>
2020-09-27 11:16:38 +0100

    Clean up test code -make use of before each/after each -abstractt table creation into own function

--- lambda-js/src/handlers.test.js
+++ lambda-js/src/handlers.test.js
@@ -1,68 +1,50 @@
 const { dynamoScan } = require("./handlers");
 const AWS = require("aws-sdk") // in package.json
 
-// example test
-test('adds 1 + 2 to equal 3', () => {
+describe('example test', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(1+2).toBe(3);
   });
+});
 
-test('dynamoScan returns all data in a table', async () => {
-    // region needs to match what's in docker compose
-    const dynamoDbClient = new AWS.DynamoDB({'region': 'eu-west-1', 'endpoint': 'http://localhost:4566' })
-    const documentClient = new AWS.DynamoDB.DocumentClient({'service': dynamoDbClient}) //sits on top od DDB client
+describe('testing dynamoScan', () => {
+  // Applies only to tests in this describe block
+  const dynamoDbClient = new AWS.DynamoDB({'region': 'eu-west-1', 'endpoint': 'http://localhost:4566' }) // region needs to match what's in docker compose
+  const documentClient = new AWS.DynamoDB.DocumentClient({'service': dynamoDbClient}) //sits on top od DDB client
+  const tableName = "MockTable"
 
-    const tableName = "MockTable"
-    const createTableParams = {
-      AttributeDefinitions: [
-         {
-        AttributeName: "Id", 
-        AttributeType: "S"
-       }
-      ], 
-      KeySchema: [
-         {
-        AttributeName: "Id", 
-        KeyType: "HASH"
-       }
-      ], 
-      ProvisionedThroughput: {
-       ReadCapacityUnits: 5, 
-       WriteCapacityUnits: 5
-      }, 
-      TableName: tableName
-     };
+  beforeEach(async () => {
+    await createTestTable(dynamoDbClient,tableName)
+    // remember to use await needed so we don't continue through the code before table has finished being made
+  });
+  
+  afterEach(async () => {
+    await dynamoDbClient.deleteTable({TableName : tableName}).promise()
+  });
 
-    // create table
-    await dynamoDbClient.createTable(createTableParams).promise()
+  
+  test('dynamoScan returns all data in a table', async () => {
+    
+    // ARRANGE - insert data
+    const insertDataParams = {
+      TableName : tableName,
+      Item: {
+        Id: 'Tom'
+      }
+    };
+    await documentClient.put(insertDataParams).promise()
 
-    try {
-
-      // insert data
-      const insertDataParams = {
-        TableName : tableName,
-        Item: {
-          Id: 'Tom'
-        }
-      };
-
-      await documentClient.put(insertDataParams).promise()
-
-      // run DynamoScan method
-      const request = {
-        TableName: tableName,
-        Limit: 5
+    // ACT - run DynamoScan method
+    const request = {
+      TableName: tableName,
+      Limit: 5
     }
+    const result = await dynamoScan(documentClient, request) //already returns a promise
 
-      const result = await dynamoScan(documentClient, request) //already returns a promise
-
-      // verify DynamoScan has returned correct data - expect statements
-      expect(result.length).toBe(1);
-      //
-    }
-    finally {
-      // Delete Table
-      await dynamoDbClient.deleteTable({TableName : tableName}).promise()
-    }
+    // ASSERT - verify DynamoScan has returned correct data - expect statements
+    expect(result.length).toBe(1);
+    
+    
 
     
     // Other scenarios to test
@@ -72,3 +54,30 @@ test('dynamoScan returns all data in a table', async () => {
 
 
 });
+
+async function createTestTable(dynamoDbClient,tableName) {
+  const createTableParams = {
+    AttributeDefinitions: [
+       {
+      AttributeName: "Id", 
+      AttributeType: "S"
+     }
+    ], 
+    KeySchema: [
+       {
+      AttributeName: "Id", 
+      KeyType: "HASH"
+     }
+    ], 
+    ProvisionedThroughput: {
+     ReadCapacityUnits: 5, 
+     WriteCapacityUnits: 5
+    }, 
+    TableName: tableName
+   };
+
+  // create table
+  await dynamoDbClient.createTable(createTableParams).promise()
+}
+});
+
