ae3c2654289d470411d8d046e4f17248bc0bb57c
Faith Ege <faithege@hotmail.co.uk>
2020-09-27 12:55:04 +0100

    Test that DynamoScan can handle empty table

--- lambda-js/src/handlers.test.js
+++ lambda-js/src/handlers.test.js
@@ -12,6 +12,7 @@ describe('testing dynamoScan', () => {
   const dynamoDbClient = new AWS.DynamoDB({'region': 'eu-west-1', 'endpoint': 'http://localhost:4566' }) // region needs to match what's in docker compose
   const documentClient = new AWS.DynamoDB.DocumentClient({'service': dynamoDbClient}) //sits on top od DDB client
   const tableName = "MockTable"
+  const dynamoScanRequest = { TableName: tableName, Limit: 2}
 
   beforeEach(async () => {
     await createTestTable(dynamoDbClient,tableName)
@@ -22,38 +23,34 @@ describe('testing dynamoScan', () => {
     await dynamoDbClient.deleteTable({TableName : tableName}).promise()
   });
 
-  
+
   test('dynamoScan returns all data in a table', async () => {
     
     // ARRANGE - insert data
-    const insertDataParams = {
-      TableName : tableName,
-      Item: {
-        Id: 'Tom'
-      }
-    };
+    const insertDataParams = { TableName : tableName, Item: { Id: 'Tom'} };
     await documentClient.put(insertDataParams).promise()
 
     // ACT - run DynamoScan method
-    const request = {
-      TableName: tableName,
-      Limit: 5
-    }
-    const result = await dynamoScan(documentClient, request) //already returns a promise
+    const result = await dynamoScan(documentClient, dynamoScanRequest) //already returns a promise
 
     // ASSERT - verify DynamoScan has returned correct data - expect statements
-    expect(result.length).toBe(1);
-    
-    
+    expect(result.length).toBe(1); //toBe used for primitives
+    expect(result).toEqual([{ Id: 'Tom' }]) //toEqual used for objects
 
-    
-    // Other scenarios to test
-    // update test above so we actually check data correct - testing base scan, update test name
-    //separate test, works with no data - testing base scan
-    //paginates correctly - above limit - as this is where recursion will kick in
+  });
 
+  test('dynamoScan returns an empty array if the table is empty', async () => {
 
-});
+    // ACT - run DynamoScan method
+    const result = await dynamoScan(documentClient, dynamoScanRequest)
+
+    // ASSERT - verify DynamoScan has returned 0 data
+    expect(result.length).toBe(0);
+
+  });
+
+  
+
 
 async function createTestTable(dynamoDbClient,tableName) {
   const createTableParams = {
