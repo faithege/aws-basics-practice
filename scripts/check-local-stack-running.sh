a18c7510dcd0715f8c2a369b56a900842571474e
Faith Ege <faithege@hotmail.co.uk>
2020-10-10 17:34:36 +0100

    Make use of subshell

--- scripts/check-local-stack-running.sh
+++ scripts/check-local-stack-running.sh
@@ -6,14 +6,15 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CONTAINER='localstack'
 
-# Use $() when need a piece of code to be evaluated before the rest oof the line is read -> not needed here
+# Use $() when need a piece of code to be evaluated before the rest of the line is read -> not needed here
 if docker ps | grep -q ${CONTAINER}
 then 
     echo "LocalStack is already running"
 else
     echo "LocalStack not currently running, setting up now"
-    cd ../scripts/
-    docker-compose up -d localstack
-    cd ../lambda-js/
+    (
+        cd ../scripts/
+        docker-compose up -d localstack
+    )
 fi
 
